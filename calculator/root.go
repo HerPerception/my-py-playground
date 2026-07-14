8edc2b75a4ba62565c036efa7bcd2258a5a839df
HerPerception <offorbuzor1@gmail.com>
2026-07-14 16:43:00 +0100

    Finding square root in Go

---
+++ calculator/root.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	numstr := os.Args[1]
+	number, err := strconv.Atoi(numstr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	numberMap := make(map[int]int)
+	i := 2
+	for number > 1 {
+		if number%i == 0 {
+			if val, ok := numberMap[i]; ok {
+				numberMap[i] = val + 1
+			} else {
+				numberMap[i] = 1
+			}
+			number /= i
+		} else {
+			i += 1
+		}
+	}
+
+	fmt.Println(numberMap)
+	root := 1
+	for key, num := range numberMap {
+		if num%2 == 0 {
+			for num > 0 {
+				root *= key
+				num -= 2
+			}
+		} else {
+			continue
+		}
+	}
+	fmt.Println(root)
+}
