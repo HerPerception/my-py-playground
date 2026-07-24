cec9cf18ea885900d40e06676a8426146f0ffd01
HerPerception <offorbuzor1@gmail.com>
2026-07-24 13:37:38 +0100

    Number-guessing with if-else and loops - control flow

---
+++ conditionals/flow.py
@@ -0,0 +1,25 @@
+for i in range(1, 10+1):
+    print(i)
+print()
+j = 1
+while j <= 10:
+    print(j)
+    j += 1
+
+for i in range(3):
+    print(i)
+else:
+    print("Done!")
+
+
+def my_func():
+    x = 10
+    print(x)
+
+x = 20
+my_func()
+print(x)
+
+text = "Python"
+print(text[1:5:2])
+
