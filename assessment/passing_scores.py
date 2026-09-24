f8933aafb37134229bd38f96f3a2ecbd67a55ad5
HerPerception <offorbuzor1@gmail.com>
2026-09-24 09:15:35 +0100

    Implementation of assessment tasks

--- assessment/passing_scores.py
+++ assessment/passing_scores.py
@@ -3,5 +3,9 @@ def passing_scores(scores):
     for index in range(len(scores)):
         if scores[index] >= 50:
             passed.append(scores[index])
-    return passed
-print(passing_scores([49, 50, 80, 65]))
+    return "Hello"#passed
+
+ passed = passing_scores([49, 50, 80, 65])
+assert isinstance(passed, list), "Passed datatype must be a list"
+
+
