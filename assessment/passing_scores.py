d9d3754efd375c51745af420910e1cc33b384a7e
HerPerception <offorbuzor1@gmail.com>
2026-09-24 13:16:07 +0100

    Assert for passing_scores.py

--- assessment/passing_scores.py
+++ assessment/passing_scores.py
@@ -3,9 +3,19 @@ def passing_scores(scores):
     for index in range(len(scores)):
         if scores[index] >= 50:
             passed.append(scores[index])
-    return "Hello"#passed
+    return passed #"Hello"
 
 passed = passing_scores([49, 50, 80, 65])
-assert isinstance(passed, list), "Passed datatype must be a list"
+try:
+    if not isinstance(passed, list):
+        raise TypeError("Datatype for passed must be a list")
+
+    for num in passed:
+        if num < 50:
+            raise ValueError("Scores must be greater than or equal to 50")
+except (TypeError, ValueError) as error:
+    print(f"Error: {error}")
+
+# assert isinstance(passed, list), "Passed datatype must be a list"
 
 
